bcf81e2625f735bf00365570e626371d384affb0
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-02-25 18:37:03 +0200

    add more webpack plugins

--- src/app.js
+++ src/app.js
@@ -2,94 +2,88 @@ import '@css/style.css';
 import '@css/background.css';
 import '@img/nav_logo.png';
 
-import _ from 'lodash';
-
-
-
 let categoryUrl = null;
 const parent = document.querySelector('.people .data');
-let currentCategory = 'people'
+let currentCategory = 'people';
 
-
-document.querySelectorAll('#button').forEach(link => {
-    link.addEventListener('click', async (event) => {
-        event.preventDefault()
-        currentCategory = link.dataset.category;
-        categoryUrl = `https://swapi.dev/api/${currentCategory}`;
-        parent.innerHTML = "";
-        document.querySelector('.load-more').classList.remove('hidden');
-        await loadPeople();
-    });
-})
+document.querySelectorAll('#button').forEach((link) => {
+  link.addEventListener('click', async (event) => {
+    event.preventDefault();
+    currentCategory = link.dataset.category;
+    categoryUrl = `https://swapi.dev/api/${currentCategory}`;
+    parent.innerHTML = '';
+    document.querySelector('.load-more').classList.remove('hidden');
+    await loadPeople();
+  });
+});
 
 document.querySelector('.load-more').addEventListener('click', async () => {
-    await loadPeople();
+  await loadPeople();
 });
 
 async function loadPeople() {
-    try {
-        const response = await fetch(categoryUrl);
-        const data = await response.json();
-        categoryUrl = data.next;
+  try {
+    const response = await fetch(categoryUrl);
+    const data = await response.json();
+    categoryUrl = data.next;
 
-        if (data.next) {
-            document.querySelector('.people').classList.remove('hidden')
-        } else {
-            document.querySelector('.load-more').classList.add('hidden')
-        }
-        showPeople(data.results);
-    } catch (error) {
-        console.error(`Loading people error: ${error}`);
+    if (data.next) {
+      document.querySelector('.people').classList.remove('hidden');
+    } else {
+      document.querySelector('.load-more').classList.add('hidden');
     }
+    showPeople(data.results);
+  } catch (error) {
+    console.error(`Loading people error: ${error}`);
+  }
 }
 
 /**
- * 
+ *
  * @param {Array[]} data
  */
 
 function showPeople(data) {
-
-    data.forEach(object => {
-        const element = document.createElement('div');
-        element.addEventListener("click", () => fetchFullDetailsFromApp(object.url));
-        if (currentCategory === 'people') {
-            element.innerHTML = `
+  data.forEach((object) => {
+    const element = document.createElement('div');
+    element.addEventListener('click', () => fetchFullDetailsFromApp(object.url));
+    if (currentCategory === 'people') {
+      element.innerHTML = `
             <p>Name: ${object.name} | Height: ${object.height} | Mass: ${object.mass} | Birth Year: ${object.birth_year}</p>
-            `
-        } else if (currentCategory === "planets") {
-            element.innerHTML = `
+            `;
+    } else if (currentCategory === 'planets') {
+      element.innerHTML = `
             <p>Name: ${object.name} | Rotation: ${object.rotation_period} | Climate: ${object.climate} | Terrain: ${object.terrain}</p>
-            `
-        } else if (currentCategory === "vehicles") {
-            element.innerHTML = `
+            `;
+    } else if (currentCategory === 'vehicles') {
+      element.innerHTML = `
             <p>Name: ${object.name} | Model: ${object.model} | Price: ${object.cost_in_credits} | Max speed: ${object.max_atmosphering_speed}</p>
-            `
-        }
-        parent.appendChild(element);
-    })
+            `;
+    }
+    parent.appendChild(element);
+  });
 }
 
 async function fetchFullDetailsFromApp(url) {
-    try {
-        const response = await fetch(url);
-        const object = await response.json();
-        showFullDetails(object);
-    } catch (error) {
-        console.error(`Fetching error: ${error}`);
-    }
+  try {
+    const response = await fetch(url);
+    const object = await response.json();
+    showFullDetails(object);
+  } catch (error) {
+    console.error(`Fetching error: ${error}`);
+  }
 }
 
 function showFullDetails(object) {
-    const content = document.querySelector('.people .data');
-    document.querySelector('.load-more').classList.add('hidden')
-    content.innerHTML = `
+  const content = document.querySelector('.people .data');
+  document.querySelector('.load-more').classList.add('hidden');
+  content.innerHTML = `
         <h3>${object.name}</h3>
         <ul>
             ${Object.entries(object)
-                .filter(([key, value]) => typeof value === "string" || typeof value === "number")
-                .map(([key, value]) => `<li><strong>${key.replace(/_/g, " ")}:</strong> ${value}</li>`)
-                .join("")}
+    .filter(([key, value]) => typeof value === 'string' || typeof value === 'number')
+    .map(([key, value]) => `<li><strong>${key.replace(/_/g, ' ')}:</strong> ${value}</li>`)
+    .join('')}
                 
         </ul>
     `;
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path, { resolve } from 'path';
 import { fileURLToPath } from 'url';
+import ESLintPlugin from 'eslint-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import ImageMinimizerPlugin from 'image-minimizer-webpack-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
@@ -22,11 +25,38 @@ export default {
     },
     module: {
         rules: [
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            {
+                test: /\.css$/,
+                use: [
+                  'style-loader',
+                  'css-loader',
+                  {
+                    loader: 'postcss-loader',
+                    options: {
+                      postcssOptions: {
+                        plugins: [
+                          ['autoprefixer', {}],
+                          ['cssnano', {}]
+                        ],
+                      },
+                    },
+                  },
+                ],
+              },
             {
                 test: /\.(png|jpe?g|svg|gif|webp)$/,
                 type: 'asset/resource',
               },
+              {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env'],
+                  }
+                }
+              }
         ]
     },
     plugins: [
@@ -34,7 +64,27 @@ export default {
         new HtmlWebpackPlugin({
             template: './src/index.html',
             favicon: './src/images/favicon.svg'
-        })
+        }),
+        new ESLintPlugin({
+            extensions: ['js'],
+            emitWarning: true,
+        }),
+        new BundleAnalyzerPlugin(),
+        new ImageMinimizerPlugin({
+            minimizer: {
+              implementation: ImageMinimizerPlugin.imageminMinify,
+              options: {
+                plugins: [
+                  ['imagemin-mozjpeg', { quality: 75 }],
+                  ['imagemin-pngquant', { quality: [0.6, 0.8] }],
+                  ['imagemin-gifsicle', { interlaced: true }],
+                  ['imagemin-svgo', { plugins: [{ removeViewBox: false }] }],
+                ],
+              },
+            },
+            exclude: /favicon.svg/,
+          }),
+        
     ],
     optimization: {
         splitChunks: {
